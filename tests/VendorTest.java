89443c6e4c7af156ac7b46aecbd91318459d4db3
Peter Hoang <peterhoang2704@gmail.com>
2024-11-21 15:01:11 -0600

    Fixed the import for the test class to properly function

--- tests/VendorTest.java
+++ tests/VendorTest.java
@@ -1,47 +1,46 @@
-import org.junit.gen5.api.Assertions;
-import org.junit.gen5.api.BeforeAll;
-import org.junit.gen5.api.Assertions;
-import org.junit.gen5.api.Test;
-import org.junit.gen5.api.BeforeEach;
-import org.junit.gen5.api.Assertions.*;
-
-class VendingTest {
-    private Vending vendingMachine;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+public class VendorTest {
+    static Vending vendingMachine;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         vendingMachine = new Vending(10, 20); // Initialize with 10 candies and 20 gums
     }
 
     @Test
-    void testAddMoney() {
+    public void testAddMoney() {
         vendingMachine.addMoney(5.00);
 
         Assertions.assertEquals(5.00, vendingMachine.getBalance());
     }
     @Test
-    void testAddNoMoney() {
+    public void testAddNoMoney() {
         vendingMachine.addMoney(0);
 
         Assertions.assertEquals(0, vendingMachine.getBalance());
     }
     @Test
-    void testAddNegativeMoney() {
+    public void testAddNegativeMoney() {
         vendingMachine.addMoney(-50);
 
         Assertions.assertEquals(0, vendingMachine.getBalance());
     }
     @Test
-    void testAddTooMuchMoney() {
+    public void testAddTooMuchMoney() {
         vendingMachine.addMoney(Integer.MAX_VALUE);
 
         Assertions.assertEquals(Integer.MAX_VALUE, vendingMachine.getBalance());
     }
     @Test
-    void testAddTooMuchMinMoney() {
+    public void testAddTooMuchMinMoney() {
         vendingMachine.addMoney(Integer.MIN_VALUE);
 
         Assertions.assertEquals(0, vendingMachine.getBalance());
     }
+
 
 }
